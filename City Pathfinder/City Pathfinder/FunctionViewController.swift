c6f5687954624e2527953e35f04ec08f3a544718
Dinh Le <tandinhlee@gmail.com>
2015-08-31 22:39:28 +0700

    Setting up the project
    First commit

---
+++ City Pathfinder/City Pathfinder/FunctionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  City Pathfinder
+//
+//  Created by Dinh Le on 31/08/15.
+//  Copyright © 2015 Dinh Le. All rights reserved.
+//
+
+import UIKit
+
+class FunctionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ City Pathfinder/City Pathfinder/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  City Pathfinder
+//
+//  Created by Dinh Le on 31/08/15.
+//  Copyright © 2015 Dinh Le. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
